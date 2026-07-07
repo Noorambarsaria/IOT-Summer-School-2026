aaa96d8dfd05f0b8868ceb012b00497592a6f4c7
Noorambarsaria <mansimarsingh98@gmail.com>
2026-07-07 23:01:25 +0530

    Add configuration header for WiFi and API settings

---
+++ Assignment_3/Q35/config.h
@@ -0,0 +1,9 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#define WIFI_SSID     "Ultra25"
+#define WIFI_PASSWORD "Noor8888"
+#define API_KEY       "f9ccde12d518e1bac0aa3ce8be2f084e"
+#define CITY_NAME     "Jammu"
+
+#endif
